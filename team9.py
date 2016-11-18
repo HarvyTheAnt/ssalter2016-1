72c4f3e37e85c545ab5704fa510bf3db49707539
HarvyTheAnt <tegilikecheese@gmail.com>
2016-11-18 09:07:42 -1000

    Update team9.py

--- team9.py
+++ team9.py
@@ -7,7 +7,7 @@
 ####
 #p.s. :this is thomas
 team_name = 'The Anthill' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
+strategy_name = 'Bullet Ants'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
